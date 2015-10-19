6f4df07ff905ff25ef7f6b24736096ad4edbb96e
Randall winters <randallwinters@Randalls-iMac-6.local>
2015-10-18 21:45:05 -0500

    Made the alg terminal friendly, since the database way took soooo long

--- dec.py
+++ dec.py
@@ -1,20 +1,19 @@
 #!/user/bin/env python3
 import itertools
-from peewee import *
 import sys
 import os
 
-db = SqliteDatabase('perms.db')
-class Perms(Model):
-	msg = CharField()
-	key = CharField()
-	class Meta:
-		database = db
+# db = SqliteDatabase('perms.db')
+# class Perms(Model):
+# 	msg = CharField()
+# 	key = CharField()
+# 	class Meta:
+# 		database = db
 
-def initialize():
-	"""Create the database and table"""
-	db.connect()
-	db.create_tables([Perms], safe=True)
+# def initialize():
+# 	"""Create the database and table"""
+# 	db.connect()
+# 	db.create_tables([Perms], safe=True)
 
 def d(c,k):
 	alphabet = "abcdefghijklmnopqrstuvwxyz1234567890 ."
@@ -34,23 +33,20 @@ def main_loop1():
 	"""Show the menu"""
 	i = 0
 	alpha = list('abcdefghijklmnopqrstuvwxyz1234567890 .')
-	listperm = list(itertools.permutations(alpha, 4))
-	perms = Perms.select()
-	delte_query = Perms.delete().where(len(str(Perms.key)) > 0)
+	listperm = list(itertools.permutations(alpha, 3))
 	while i < len(listperm):
 		string_key = ''.join(listperm[i])
 		# print(string_key)
 		decrypted_message = d('2vy9xq7vg0g6upfgtamtw2ilhgwwtau0s9uptvfvu0tcegtazg3 tmog8hflgptgnmrvvm3xhr.jxehtav wp3rehmbpfsds2pn0w6dcaguw103 s9gq7gg03  .fq7ifs2v dbkaa1gu  wp3ryvmbpetfzrdl0uslc.28ayduu riq.a2m6vsq.qrflb3ue9uwruonepir.28a6ncpsm.78wfv95t9gq7ffnc  uiiaap0aribag7q10byar.78wfrctk9b2revc35g9cmvc2ryptfb2.ahm86ygoo', string_key)
-		Perms.create(msg=decrypted_message, key=string_key)
-		i += 1
-	perms = Perms.select()
-	perms = perms.where(Perms.msg.contains("fuck"))
-	for perm in perms:
-		print (perm.msg)
-		print (perm.key)
+		if decrypted_message.find('fuck') != -1:
+			print (decrypted_message)
+			print (string_key)
+		
 
+		i += 1
+		
 
 
 if __name__ == '__main__':
-	initialize()
+	# initialize()
 	main_loop1()
